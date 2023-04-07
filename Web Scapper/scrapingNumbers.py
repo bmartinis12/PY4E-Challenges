9db451ae1f5c7acc09753e8129c4e726887c5cc7
Benjamin Martinis <martinisbenjamin@gmail.com>
2023-04-07 14:42:56 -0500

    Create scrapingNumbers.py

---
+++ Web Scapper/scrapingNumbers.py
@@ -0,0 +1,20 @@
+from urllib.request import urlopen
+import collections
+collections.Callable = collections.abc.Callable
+from bs4 import BeautifulSoup
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+
+sum = 0
+tags = soup('span')
+for tag in tags:
+    sum = sum + float(tag.contents[0])
+
+print("Sum:", sum)
